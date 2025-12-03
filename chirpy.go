6a1d839091d73ac4b7955e3f7dc4c33168cbefb1
joshckidd <josh@larameekidd.com>
2025-12-03 12:58:29 -0500

    sdded custom handler

---
+++ chirpy.go
@@ -0,0 +1,9 @@
+package main
+
+import "net/http"
+
+func readinessEndpoint(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import "net/http"
 
 func main() {
 	serveMux := http.NewServeMux()
+	serveMux.HandleFunc("/healthz", readinessEndpoint)
 	server := http.Server{
 		Handler: serveMux,
 		Addr:    ":8080",
 	}
 
-	serveMux.Handle("/", http.FileServer(http.Dir("/home/josh/Documents/repos/github.com/joshckidd/chirpy")))
+	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("/home/josh/Documents/repos/github.com/joshckidd/chirpy"))))
 	server.ListenAndServe()
 }
